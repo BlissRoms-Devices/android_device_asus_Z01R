8110173a3cea9f01e0a63b9b05ecc9e4ab0d8cf2
Aryan Arora <arora.aryan7@gmail.com>
2020-04-26 21:57:09 +0530

    Z01R: Enable TARGET_INCLUDE_WIFI_EXT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -176,5 +176,8 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_Z01R
 TARGET_RECOVERY_DEVICE_MODULES := libinit_Z01R
 
+# wifi
+TARGET_INCLUDE_WIFI_EXT := true
+
 # Inherit from the proprietary version
 -include vendor/asus/Z01R/BoardConfigVendor.mk
