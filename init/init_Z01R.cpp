cc8a6bf82c957d718262ac39fe086fe8a1611093
Aryan Arora <arora.aryan7@gmail.com>
2021-07-11 15:45:50 +0000

    Update FP to July patch

--- init/init_Z01R.cpp
+++ init/init_Z01R.cpp
@@ -56,6 +56,6 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 void vendor_load_properties()
 {
     // fingerprint
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210605.005 7349499 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
 }
